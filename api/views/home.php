96cf4ba1925350414bf874afc137263df821154f
ianodhiambo1 <ian.odhiambo@strathmore.edu>
2023-11-30 18:03:32 +0300

    hero page changes

--- api/views/home.php
+++ api/views/home.php
@@ -33,7 +33,7 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
                     </svg>
                 </li>
-                <li><a class="text-sm text-blue-600 font-bold" href="#">Your Profile</a></li>
+                <li><a class="text-sm text-blue-600 font-bold" href="/index.php/user/profile">Your Profile</a></li>
                 <li class="text-gray-300">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" class="w-4 h-4 current-fill" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
@@ -108,11 +108,10 @@
                             </svg>
                             <span class="relative">The</span>
                         </span>
-                        quick, brown fox jumps over a lazy dog
+                        Matadrug API 
                     </h2>
                     <p class="text-base text-gray-700 md:text-lg">
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem
-                        aperiam, eaque ipsa quae.
+                    The API product provides developers with seamless access to robust data analytics, enabling advanced insights and actionable intelligence. With user-friendly endpoints and secure authentication, it empowers applications to deliver innovative, data-driven solutions.
                     </p>
                 </div>
                 <div>
